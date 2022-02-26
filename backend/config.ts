9fa5f988e14cd4688ce31a02f0ba0f5716b99339
Petr Hurtak <petr.hurtak@gmail.com>
2022-02-26 22:37:19 +0100

    ci

--- backend/config.ts
+++ backend/config.ts
@@ -1,7 +1,16 @@
-const isProduction = process.env.NODE_ENV === "production"
+import { z } from "zod"
+
+const env = z
+	.object({
+		NODE_ENV: z.string().optional(),
+		PORT: z.number().optional(),
+	})
+	.parse(process.env)
+
+const isProduction = env.NODE_ENV === "production"
 
 export const config = {
-	port: process.env.PORT || 3000,
+	port: env.PORT ?? 3000,
 	isProduction,
 	isDev: !isProduction,
 }
